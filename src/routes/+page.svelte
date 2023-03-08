c34c5d2378e408adc37f059e4784f8f84f0288d0
carloshatus <carlos.hatus@hotmail.com>
2023-03-08 19:15:03 -0300

    change to svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<div class="container">
+  <h1>Carlos Hatus</h1>
+</div>
+
+<style>
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    width: 100%;
+    height: 100vh;
+  }
+  h1 {
+    font-size: 8em;
+    font-weight: 800;
+    font-family: roboto, sans-serif;
+    text-transform: uppercase;
+    background: -webkit-linear-gradient(230deg, #50628f, #160c58);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+</style>
